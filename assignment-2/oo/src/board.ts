e01f29c9b76b22a3f05450aebfeb15a724eee4e1
Ioana Estera Dumitrache <ide@novicell.dk>
2022-10-23 20:01:15 +0200

    Working with moves

--- assignment-2/oo/src/board.ts
+++ assignment-2/oo/src/board.ts
@@ -1,3 +1,5 @@
+
+
 export type Generator<T> = { next: () => T };
 
 export type Position = {
@@ -44,9 +46,20 @@ export class Board<T> {
   canMove(first: Position, second: Position): boolean {
     return this.isMoveLegal(first, second);
     // return false;
+    if (first.col == second.col || first.row == second.row) 
+        {
+            this.swapPieces(first, second);
+            let m = this.isMatch(first, second);
+            this.swapPieces(first, second);
+            return m;      
+        }
+        return false;
   }
 
-  move(first: Position, second: Position) {}
+  move(first: Position, second: Position) {
+    // return this.checkIfPiecesMatch(first,second);
+    
+  }
 
   /* -------------------------------------------------------------------------- */
   /*                                   HELPERS                                  */
@@ -110,7 +123,7 @@ export class Board<T> {
 
   private isMatch() {}
 
-  private findMatchInRows(row?: number) {
+   findMatchInRows(row?: number) {
     if (row > this.height) {
       return;
     }
@@ -145,8 +158,46 @@ export class Board<T> {
     return result;
   }
 
-  private findMatchInColumns(column: number) {}
+  private findMatchInColumns(rowI:boolean, num:number) {
+    let match = false;
+    const p = this.pieces.filter((element) => {
+      if(rowI){
+        return element.position.row == num;
+      }
+      else {
+        return element.position.col == num;
+      }
+      
+    });
 
+    for (var j = 0; j < p.length; j++){
+      if (j == 0){
+        if(p[j].value == p[j+1].value && p[j].value == p[j+2].value)
+        {
+          match = true;
+          break;
+        }
+      }
+      else if (j > 0 && j != (p.length - 1)){
+        if(p[j].value == p[j+1].value && p[j].value == p[j-1].value)
+        {
+          match = true;
+          break;
+        }
+      }
+    }
+    return match;
+  }
+
+  private isMatch(first: Position, second: Position)
+  {
+    let isFirstHorizontalMatch = this.findMatch(first.row, true);
+    let isSecondHorizontalMatch = this.findMatch(second.row, true);
+    let isFirstVerticalMatch = this.findMatch(first.col, false);
+    let isSecondVerticalMatch = this.findMatch(second.col, false);
+    let match = (isFirstHorizontalMatch || isSecondHorizontalMatch || isFirstVerticalMatch || isSecondVerticalMatch)
+    return match;
+  }
   /* -------------- Checks if given position is outside the board ------------- */
 
   private isPositionOutsideBoard(p: Position): boolean {
@@ -189,22 +240,74 @@ export class Board<T> {
 
 /* ----------------- Check if pieces match ---------------- */
 
-private checkIfPiecesMatch(board: Board, position:Position): boolean 
-{
-  const top1 = {x: position.x - 1, y: position.y};
-  const top2 = {x: position.x - 2, y: position.y};
-  const bottom1 = {x: position.x + 1, y: position.y};
-  const bottom2 = {x: position.x + 2, y: position.y};
+// private checkIfPiecesMatch(board: Board, position:Position): boolean 
+// {
+//   const top1 = {x: position.x - 1, y: position.y};
+//   const top2 = {x: position.x - 2, y: position.y};
+//   const bottom1 = {x: position.x + 1, y: position.y};
+//   const bottom2 = {x: position.x + 2, y: position.y};
 
-  const left1 = {x: position.x, y: position.y - 1};
-  const left2 = {x: position.x, y: position.y - 2};
-  const right1 = {x: position.x, y: position.y + 1};
-  const right2 = {x: position.x, y: position.y + 2};
+//   const left1 = {x: position.x, y: position.y - 1};
+//   const left2 = {x: position.x, y: position.y - 2};
+//   const right1 = {x: position.x, y: position.y + 1};
+//   const right2 = {x: position.x, y: position.y + 2};
   
-//   if (board.doCandiesMatch (position, right1) && board.doCandiesMatch(cellPos, right2)) {
+//   if (board.findMatchInRows (position, right1) && board.findMatchInRows(position, right2)) {
 //     return true;
 // }
-}
+//  // Check if horizontal match with this cell in middle.
+//  if (board.findMatchInRows (position, left1) && board.findMatchInRows(position, right1)) {
+//   return true;
+// }
+
+// // Check if horizontal match with this cell in right.
+// if (board.findMatchInRows (position, left1) && board.findMatchInRows(position, left2)) {
+//   return true;
+// }
+
+// // Check if vertical match with this cell in top.
+// if (board.findMatchInRows (position, bottom1) && board.findMatchInRows (position, bottom2)) {
+//   return true;
+// }
+
+// // Check if vertical match with this cell in middle.
+// if (board.findMatchInRows (position, top1) && board.findMatchInRows (position, bottom1)) {
+//   return true;
+// }
+
+// // Check if vertical match with this cell in bottom.
+// if (board.findMatchInRows (position, top1) && board.findMatchInRows (position, top2)) {
+//   return true;
+// }
+
+// return false;
+// }
+
+// // ----------------------------------------------------------------------------------
+
+// private comparePositions (pos1: Position, pos2: Position): boolean {
+//   return (pos1=== pos2);
+// }
+
+//  doMatch (first: Position, second:Position): boolean {
+//   if (first === second) {
+//       return true;
+//   }
+
+//   return false;
+// }
+
+// refill(): void {
+//   for (let x = 0; x < this.width; x++) {
+//     for (let y = this.height - 1; y >= 0; y--) {
+//       const cell = this.at({ x, y });
+
+//       if (cell.empty) {
+//         cell.set(this.generator());
+//       }
+//     }
+//   }
+// }
 
 
 
