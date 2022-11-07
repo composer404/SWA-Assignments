97d0cc986c513f2ba3c180151de2ab217715d9b2
Estera26 <dumi_estera@yahoo.com>
2022-11-07 18:23:27 +0100

    Setup

--- assignment-3/src/App.tsx
+++ assignment-3/src/App.tsx
@@ -1,57 +1,32 @@
-import './App.css';
+import './App.css'
 
-import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { useEffect, useState } from 'react'
 
-function App() {
+import { Container } from 'react-bootstrap'
+import { useNavigate } from "react-router-dom"
+
+function App () {
+  let navigate = useNavigate();
+  function handleClick() {
+    navigate("/home");
+}
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
+    // <BrowserRouter>
+    //   <Header />
+    //   <main>
+    //     <Container>
+    //       <Route path='/' element={<HomePage/>} />
+    //       <Route path='/signUp' component={SignUpPage} />
+    //       <Route path='/login' component={LoginPage} />
+    //     </Container>
+    //   </main>
+    //   <Footer />
+    // </BrowserRouter>
+    <div>
+      <button onClick={handleClick}>go home</button>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ assignment-3/src/store.tsx
@@ -0,0 +1,29 @@
+// import { applyMiddleware, combineReducers, createStore } from 'redux'
+
+// import { composeWithDevTools } from 'redux-devtools-extension'
+// import thunk from 'redux-thunk'
+// import { userLoginReducer } from './reducers/userReducer'
+
+// const reducers = combineReducers({
+//   userLogin: userLoginReducer,
+// })
+
+// const userInfoFromStorage = localStorage.getItem('userInfo')
+//   ? JSON.parse(localStorage.getItem('userInfo')!)
+//   : undefined
+
+// const initialState = {
+//   userLogin: { userInfo: userInfoFromStorage },
+// } as {}
+
+// const middleware = [thunk]
+
+// const store = createStore(
+//   reducers,
+//   initialState,
+//   composeWithDevTools(applyMiddleware(...middleware))
+// )
+
+// export type RootState = ReturnType<typeof store.getState>
+
+// export default store
