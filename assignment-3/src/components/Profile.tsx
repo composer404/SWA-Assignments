cceaf0a0a9111c14c87178a481c8fb82e230ee7f
Oliwer Szefer <oszefer@man.poznan.pl>
2022-11-11 17:17:56 +0100

    Updated the profile component for the user

--- assignment-3/src/components/Profile.tsx
+++ assignment-3/src/components/Profile.tsx
@@ -1,38 +1,117 @@
-import { Navigate } from 'react-router-dom';
-import React from "react";
-import { useSelector } from "react-redux";
+import {Navigate, useNavigate} from 'react-router-dom';
+import React, {useEffect, useState} from "react";
+import {useDispatch, useSelector} from "react-redux";
+import {updateUserProfile} from "../actions/auth";
 
 const Profile = () => {
-  const { isLoggedIn } = useSelector((state: any) => state.auth);
 
-  if (!isLoggedIn) {
-    return <Navigate to="/login" />;
+  let navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const { isUpdated } = useSelector((state: any) => state.auth);
+  const { message } = useSelector((state: any) => state.message);
+
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const [invalidPassword, setInvalidPassword] = useState(true);
+  const [invalidUsername, setInvalidUsername] = useState(true);
+
+  const [successful] = useState(false);
+
+  useEffect(() => {
+    dispatch(updateUserProfile(isUpdated) as any);
+  });
+
+  const onChangeUsername = (e: any) => {
+    validateUsername(e);
+    const username = e.target.value;
+    setUsername(username);
+  };
+
+  const onChangePassword = (e: any) => {
+    validatePassword(e)
+    const password = e.target.value;
+    setPassword(password);
+  };
+
+  const validateUsername = (e: any) => {
+    if(e.target.value.length < 3 || e.target.value.length > 12) {
+      setInvalidUsername(true);
+      return;
+    }
+    setInvalidUsername(false);
+  }
+
+  const validatePassword = (e: any) => {
+    if(e.target.value.length < 3 || e.target.value.length > 12) {
+      setInvalidPassword(true);
+      return;
+    }
+    setInvalidPassword(false);
+  }
+
+  if (isUpdated) {
+    return <Navigate to="/game" />;
   }
 
   return (
-    <div>Profile:</div>
-    // <div className="container">
-    //   <header className="jumbotron">
-    //     <h3>
-    //       <strong>{currentUser.username}</strong> Profile
-    //     </h3>
-    //   </header>
-    //   <p>
-    //     <strong>Token:</strong> {currentUser.accessToken.substring(0, 20)} ...{" "}
-    //     {currentUser.accessToken.substr(currentUser.accessToken.length - 20)}
-    //   </p>
-    //   <p>
-    //     <strong>Id:</strong> {currentUser.id}
-    //   </p>
-    //   <p>
-    //     <strong>Email:</strong> {currentUser.email}
-    //   </p>
-    //   <strong>Authorities:</strong>
-    //   <ul>
-    //     {currentUser.roles &&
-    //       currentUser.roles.map((role: any, index:any) => <li key={index}>{role}</li>)}
-    //   </ul>
-    // </div>
+      <div className="col-md-4 mx-auto h-100">
+        <div className="card card-container p-4 my-auto">
+          <img
+              src="//ssl.gstatic.com/accounts/ui/avatar_2x.png"
+              alt="profile-img"
+              className="profile-img-card"
+          />
+          {(
+              <div>
+                <div className="form-group mt-2">
+                  <label htmlFor="username">Username</label>
+                  <input
+                      type="text"
+                      className="form-control"
+                      name="username"
+                      value={username}
+                      onChange={onChangeUsername}
+                  />
+                </div>
+
+                {invalidUsername}
+
+                <div className="form-group mt-2">
+                  <label htmlFor="password">Password</label>
+                  <input
+                      type="password"
+                      className="form-control"
+                      name="password"
+                      value={password}
+                      onChange={onChangePassword}
+                  />
+                </div>
+
+                {invalidPassword}
+
+                <div className="form-group mt-4">
+                  <button disabled={invalidPassword || invalidUsername} className="btn btn-primary btn-block w-100">Save</button>
+                </div>
+              </div>
+          )}
+
+          {message && successful && (
+              <Navigate to="/" />
+          )}
+
+          {message && !successful && (
+              <div className="form-group mt-4">
+                <div className="alert alert-danger" role="alert">
+                  <div>Incorrect username or password.</div>
+                  <div>{message}</div>
+                </div>
+              </div>
+          )}
+        </div>
+      </div>
+
   );
 };
 
