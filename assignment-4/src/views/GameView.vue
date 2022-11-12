54fcacb803d5709877f7e25a4d2ccf1328f8aec1
Kozik Przemysław <przemyk1115@gmail.com>
2022-11-12 16:58:58 +0100

    Added game server, service, utils and support for typescirpt

--- assignment-4/src/views/GameView.vue
+++ assignment-4/src/views/GameView.vue
@@ -1,5 +1,30 @@
 <template>
   <div>
+    <button @click="createBoard">Create</button>
     <h1>Game</h1>
   </div>
 </template>
+
+<script>
+  import { create, initalScan } from '@/services/game.service'
+  import { RandomColorGenerator } from '@/utils/generator'
+
+  export default {
+    data() {
+      return {
+        username: '',
+        password: '',
+      }
+    },
+    methods: {
+      createBoard() {
+        const generator = new RandomColorGenerator();
+
+        // Clears all matches before save in state
+        const initBoard = create(generator, 4, 4);
+        const { board } = initalScan(generator, initBoard);
+        console.log(`board`, board);
+      }
+    }
+  }
+</script>
